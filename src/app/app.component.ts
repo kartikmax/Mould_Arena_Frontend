7fdd382fe18523576a8ed47ef66476004140a9f1
Ashish <ashish2017pandey@gmail.com>
2023-10-29 17:31:14 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mould_Arena_Project';
+  images: string[] = ['C:\Users\hp4\Downloads\WhatsApp Image 2023-10-22 at 12.48.47 AM.jpeg', 'image2.jpg', 'image3.jpg'];
+}
+
+
+
+  
+
